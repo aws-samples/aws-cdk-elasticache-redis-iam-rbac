acbe509c831eb566a55b1c89adb1c8b6e2b654a4
Mirus Lu <miruslu@amazon.com>
2021-02-16 09:51:39 -0800

    Solved: password on ECUser wasn't being resolved properly

--- lib/redis-rbac-custom-resource.ts
+++ lib/redis-rbac-custom-resource.ts
@@ -59,49 +59,12 @@ export class RedisRbacUser extends cdk.Construct {
       userName: props.redisUserName,
       accessString: props.accessString? props.accessString : "off +get ~keys*", // Mirus Todo: File a bug: this is required even though the docs say that it isn't -- result is 500 internal error on service ElastiCache
       userId: props.redisUserId,
-      passwords: [this.rbacUserSecret.secretValue.toString()]
+      passwords: [this.rbacUserSecret.secretValueFromJson('password').toString()]
     })
 
 
+    user.node.addDependency(this.rbacUserSecret)
 
-
-    // Create a role for the Lambda
-    // const rbacCustomResourceRole = new iam.Role(this, 'RbacCR-'+props.redisUserName, {
-    //   assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
-    //   description: 'Role to be assumed by mock application lambda',
-    // });
-
-    // rbacCustomResourceRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambdaBasicExecutionRole"));
-    // rbacCustomResourceRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambdaVPCAccessExecutionRole"));
-
-    // rbacUserSecret.grantRead(rbacCustomResourceRole)
-
-
-    // const onEvent = new lambda.Function(this, 'onEventHandler', {
-    //   runtime: lambda.Runtime.PYTHON_3_7,
-    //   handler: 'custom_resource_handler.lambda_handler',
-    //   code: lambda.Code.fromAsset(path.join(__dirname, 'lambda/rbac_cr.zip')),
-    //   // layers: [redis_py_layer],
-    //   role: rbacCustomResourceRole,
-    //   vpc: props.vpc,
-    //   vpcSubnets: {subnetType: ec2.SubnetType.PRIVATE},
-    //   securityGroups: props.elastiCacheSecurityGroups,
-    //   timeout: cdk.Duration.seconds(10),
-    //   environment: {
-    //     redis_endpoint: props.elastiCacheReplicationGroup.attrPrimaryEndPointAddress,
-    //     redis_port: props.elastiCacheReplicationGroup.attrPrimaryEndPointPort,
-    //     secret_arn: rbacUserSecret.secretArn,
-    //     redis_username: props.redisUserName
-    //   }
-    // });
-
-    // const rbacUserProvider = new customResource.Provider(this, "RbacUserProvider", {
-    //   onEventHandler: onEvent
-    // });
-
-    // new cdk.CustomResource(this, props.redisUserName, {
-    //   serviceToken: rbacUserProvider.serviceToken
-    // })
   }
 
 }
