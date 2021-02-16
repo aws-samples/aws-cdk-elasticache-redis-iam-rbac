d8abb324fe7fec058db177591d166e83f42f366c
Mirus Lu <miruslu@amazon.com>
2021-02-15 18:08:05 -0800

    Enabled ssl on mock app

--- lib/lambda/redis_connect.py
+++ lib/lambda/redis_connect.py
@@ -12,7 +12,8 @@ def lambda_handler(event, context):
 
     redis_server = redis.Redis(
         host=os.environ['redis_endpoint'],
-        port=os.environ['redis_port'])
+        port=os.environ['redis_port'],
+        ssl=True)
 
     redis_server.set("name", "orkb")
     result = redis_server.get("name")
